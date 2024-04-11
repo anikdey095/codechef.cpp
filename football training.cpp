096877782edecd2defdcad2a27689af660de0586
anikdey095 <157403107+anikdey095@users.noreply.github.com>
2024-04-11 10:38:57 +0600

    Create football training.cpp

---
+++ football training.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int f, p;
+    cin >> f >> p;
+    if (f > p)
+    {
+        cout << "FREEKICK" << endl;
+    }
+    else
+    {
+        cout << "PENALTY" << endl;
+    }
+}
